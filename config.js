695207c045a373ceeb26e21f3328106e8e3ac048
Orens Xhagolli <orens.xhagolli@hotmail.com>
2019-05-31 16:41:22 -0700

    Fix prefix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: ``, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `op-podcast`, // This path is subpath of your hosting https://domain/portfolio
   // social
   heading: 'The OP Podcast',
   subHeading:
